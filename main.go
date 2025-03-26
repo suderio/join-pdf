cb95bdda2d08a293e4ddfcf711c9434ec4c0608f
Paulo Suderio <paulo.suderio@gmail.com>
2025-03-25 23:03:57 -0300

    gitignore e versão da aplicação

--- main.go
+++ main.go
@@ -6,26 +6,44 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
+	"fyne.io/fyne/v2/widget"
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
 	"github.com/schollz/progressbar/v3"
 )
 
-var version = "dev" // será sobrescrito pelo goreleaser
+var version = "0.1.0"
 
 func main() {
-	// Flags
+	guiMode := flag.Bool("gui", false, "Abrir interface gráfica")
 	outputName := flag.String("name", "output.pdf", "Nome do PDF final")
 	flag.StringVar(outputName, "n", "output.pdf", "Nome do PDF final (atalho)")
+	showVersion := flag.Bool("version", false, "Exibir versão")
+
 	flag.Parse()
 
+	if *showVersion {
+		fmt.Println("join-pdf", version)
+		return
+	}
+
+	if *guiMode {
+		runGUI()
+		return
+	}
+
 	if flag.NArg() == 0 {
-		fmt.Println("Erro: forneça pelo menos um arquivo PDF de entrada (pode usar globs)")
+		fmt.Println("Erro: forneça arquivos PDF para mesclar (ou use --gui)")
 		os.Exit(1)
 	}
 
-	// Resolver globs
 	files := flag.Args()
 	var pdfPaths []string
 	var notFound []string
@@ -45,7 +63,7 @@ func main() {
 	}
 
 	if len(notFound) > 0 {
-		fmt.Println("Erro: os seguintes arquivos não foram encontrados:")
+		fmt.Println("Erro: arquivos não encontrados:")
 		for _, f := range notFound {
 			fmt.Printf(" - %s\n", f)
 		}
@@ -57,20 +75,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Barra de progresso
 	bar := progressbar.NewOptions(len(pdfPaths),
 		progressbar.OptionSetDescription("Mesclando PDFs"),
 		progressbar.OptionShowCount(),
-		progressbar.OptionSetPredictTime(true),
-		progressbar.OptionSetTheme(progressbar.Theme{
-			Saucer:        "=",
-			SaucerPadding: "-",
-			BarStart:      "[",
-			BarEnd:        "]",
-		}),
+		progressbar.OptionSetTheme(progressbar.Theme{Saucer: "=", BarStart: "[", BarEnd: "]"}),
 	)
 
-	// Verifica se os arquivos existem e são PDFs
 	for _, path := range pdfPaths {
 		info, err := os.Stat(path)
 		if err != nil || info.IsDir() {
@@ -79,7 +89,6 @@ func main() {
 		bar.Add(1)
 	}
 
-	// Merge com pdfcpu
 	conf := model.NewDefaultConfiguration()
 	err := api.MergeCreateFile(pdfPaths, *outputName, false, conf)
 	if err != nil {
@@ -90,5 +99,60 @@ func main() {
 }
 
 func containsGlob(s string) bool {
-	return filepath.Base(s) != filepath.Clean(s)
+	return strings.ContainsAny(s, "*?[")
+}
+
+// ---------------- GUI MODE ----------------
+
+func runGUI() {
+	a := app.New()
+	w := a.NewWindow("PDF Joiner")
+
+	var pdfFiles []string
+	list := widget.NewMultiLineEntry()
+	list.SetPlaceHolder("Nenhum arquivo selecionado.")
+
+	btnSelect := widget.NewButton("Selecionar PDFs", func() {
+		fd := dialog.NewFileOpen(func(r fyne.URIReadCloser, err error) {
+			if err != nil || r == nil {
+				return
+			}
+			path := r.URI().Path()
+			pdfFiles = append(pdfFiles, path)
+			list.SetText(strings.Join(pdfFiles, "\n"))
+		}, w)
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".pdf"}))
+		fd.Resize(fyne.NewSize(800, 600))
+		fd.Show()
+	})
+
+	btnMerge := widget.NewButton("Salvar PDF Final", func() {
+		if len(pdfFiles) == 0 {
+			dialog.ShowInformation("Aviso", "Nenhum PDF selecionado.", w)
+			return
+		}
+
+		dialog.NewFileSave(func(writer fyne.URIWriteCloser, err error) {
+			if err != nil || writer == nil {
+				return
+			}
+			output := writer.URI().Path()
+			conf := model.NewDefaultConfiguration()
+			err = api.MergeCreateFile(pdfFiles, output, false, conf)
+			if err != nil {
+				dialog.ShowError(err, w)
+			} else {
+				dialog.ShowInformation("Sucesso", "PDF criado com sucesso!", w)
+			}
+		}, w).Show()
+	})
+
+	w.SetContent(container.NewVBox(
+		widget.NewLabel("Selecionador de PDFs"),
+		btnSelect,
+		list,
+		btnMerge,
+	))
+	w.Resize(fyne.NewSize(500, 400))
+	w.ShowAndRun()
 }
